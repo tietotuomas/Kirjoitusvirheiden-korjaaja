f16aa856a090f89041281c87317835574c74860f
Tuomas <tietotuomas@gmail.com>
2022-04-08 20:09:03 +0300

    Update trie structure

--- src/services/trie.py
+++ src/services/trie.py
@@ -1,20 +1,41 @@
-class Trie:
+class TrieSolmu:
+    """
+    Luokka mallintaa Trie-tietorakenteen (puun) solmua. Ensimmäinen solmu toimii puun juurena. 
+    Self.sana sisältää sanan vain jos jokin sana päättyy ko. solmuun. 
+    """
     def __init__(self):
-        self.juuri = {"*": "*"}
+        self.sana = None
+        self.lapset = {}
 
-    def lisaa_sana(self, sana):
-        solmu = self.juuri
+    def lisaa_sana(self, sana: str):
+        """
+        Lisätään sana puuhun, kirjain (haara) kerrallaan. Viimeisen kirjaimen kohdalla merkataan sana.
+        """
+        solmu = self
         for kirjain in sana:
-            if kirjain not in solmu:
-                solmu[kirjain] = {}
+            if kirjain not in solmu.lapset:
+                solmu.lapset[kirjain] = TrieSolmu()
 
-            solmu = solmu[kirjain]
-        solmu["*"] = "*"
+            solmu = solmu.lapset[kirjain]
 
-    def onko_sana_olemassa(self, sana):
-        solmu = self.juuri
+        solmu.sana = sana
+
+    def onko_sana_olemassa(self, sana: str):
+        """
+        Tarkistaa, löytyykö sana puusta.
+        Palauttaa True, jos löytyy. Muuten False.
+        """
+        solmu = self
         for kirjain in sana:
-            if kirjain not in solmu:
+            if not kirjain in solmu.lapset:
                 return False
-            solmu = solmu[kirjain]
-        return "*" in solmu
+            solmu = solmu.lapset[kirjain]
+        if solmu.sana:
+            return True
+        return False
+
+    
+    def __repr__(self) -> str:
+        if self.sana:
+            return f"{self.lapset}, sana: {self.sana}"
+        return f"{self.lapset}"
